2f01b1879bc57aa0afdb83f8d8daa20f423d8f4a
Uchio KONDO <udzura@pepabo.com>
2015-09-24 18:24:56 +0900

    Add dest_path config value
    - e.g.
        {
          "type": "serverspec",
          "dest_path": "./packer/serverspec"
        }

--- provisoner.go
+++ provisoner.go
@@ -1,20 +1,53 @@
 package serverspec
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
+	"github.com/mitchellh/packer/common"
+	"github.com/mitchellh/packer/helper/config"
 	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/template/interpolate"
 )
 
+type Config struct {
+	common.PackerConfig `mapstructure:",squash"`
+
+	DestPath string `mapstructure:"dest_path"`
+
+	ctx interpolate.Context
+}
+
 type Provisioner struct {
+	config Config
 }
 
 func (p *Provisioner) Prepare(raws ...interface{}) error {
+	err := config.Decode(&p.config, &config.DecodeOpts{
+		Interpolate:        true,
+		InterpolateContext: &p.config.ctx,
+	}, raws...)
+	if err != nil {
+		return err
+	}
+
+	var errs *packer.MultiError
+	if p.config.DestPath == "" {
+		errs = packer.MultiErrorAppend(errs,
+			errors.New("`dest_path' must be specified."))
+	}
+
+	if errs != nil && len(errs.Errors) > 0 {
+		return errs
+	}
 	return nil
 }
 
 func (p *Provisioner) Provision(ui packer.Ui, comm packer.Communicator) error {
 	ui.Say("Plugin test")
+	ui.Say(fmt.Sprintf("DestPath is: %s", p.config.DestPath))
+
 	return nil
 }
 
